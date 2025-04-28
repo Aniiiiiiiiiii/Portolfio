6f0ae49999e828c699ccde4d6a557b51a5c59359
Annwesha <143275509+Aniiiiiiiiiii@users.noreply.github.com>
2025-04-28 13:11:58 +0530

    Update About.jsx
    Added about content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,15 @@ export default function About(){
                 </div>
                 <div className="col-12 col-sm-6">
                     <h2 className="mb-4 nunito-bold sec-head" >&#123;About Me&#125;</h2>
-                    <p className="text-justify nunito-exlight">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus accusantium sapiente aliquam obcaecati reprehenderit dolorem molestiae! Beatae necessitatibus error placeat impedit dolorum architecto modi expedita fuga debitis, earum numquam neque nobis in autem dignissimos ad libero quae. Quisquam quos architecto nam laborum dolorum voluptatibus quo! Eveniet velit blanditiis inventore maiores? Modi eaque molestiae ea perferendis distinctio dignissimos placeat alias velit enim nemo eius dolor omnis praesentium facere, aliquam eveniet et repellendus, quod voluptas nostrum necessitatibus? Expedita, labore a? Illum, voluptatem! Earum mollitia esse eligendi, odio harum magnam consequatur explicabo incidunt corporis perferendis inventore aliquid! Natus aspernatur quod expedita suscipit. Ab. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates repellendus est aut aspernatur quibusdam, reiciendis odio libero deserunt! Veritatis quod quasi suscipit quidem amet, rem quas ut doloremque et dolore repellendus tempore. Mollitia voluptas odio sapiente modi quis laudantium, enim reiciendis doloremque similique voluptatem ea alias nisi quibusdam quam iste, praesentium eius nesciunt repellendus voluptates provident sequi ipsam reprehenderit. Excepturi adipisci repellat odit hic consectetur! Saepe dolore est quod eos sunt omnis, dolorum dignissimos aperiam quo non harum placeat, impedit nihil sit natus architecto. Quidem sit veniam ut non molestias. </p>
+                    <p className="text-justify nunito-exlight">I am a passionate Front-End developer who transitioned from a background in Zoology (Hons) to the exciting world of technology. A deep curiosity drove my journey into tech to understand systems and problem-solving. I discovered my love for coding and the endless possibilities it offers, which led me to explore web development.
+
+With hands-on experience in CodeIgniter for Back-End development and React for Front-End integration, I specialize in building dynamic, responsive, and user-friendly web applications with the latest and most in-demand technologies. My learning phase has instilled a keen attention to detail, analytical thinking, and a systematic approach to development, all of which play a significant role in how I approach coding and problem-solving.
+
+During my time at Krold IT Solutions and my internship at Technosoft Academy, I’ve had the opportunity to collaborate with diverse teams, working on exciting projects for domestic as well as international clients. I take pride in delivering high-quality web and app solutions, optimizing user experiences, and creating seamless interactions that make websites both functional and beautiful.
+
+I’m always excited to learn new technologies, improve my skills, and contribute to projects that solve real-world problems. As a developer, my goal is to continue growing and evolving in this ever-changing tech landscape, while always keeping the user at the heart of my work.
+
+</p>
                 </div>
             </div>
         </section>
